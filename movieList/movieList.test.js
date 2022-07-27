bb44fa43905ffc1cce18b5e3778ea0a0c02ef782
ishakrishangi <krishangik@gmail.com>
2022-07-27 12:25:03 -0400

    updated things to be in their own test file

--- movieList/movieList.test.js
+++ movieList/movieList.test.js
@@ -4,7 +4,7 @@ require('chromedriver')
 
 const driver = new Builder().withCapabilities(Capabilities.chrome()).build()
 
-beforeEach(async() => {
+beforeAll(async() => {
     await driver.get('http://localhost:5500/movieList/index.html')
 })
 
@@ -13,36 +13,74 @@ afterAll( async() => {
 })
 
 
-describe('movie-list tests', () => {
-
-    test('check if movie is added to the list', async() => {
-        //selecting the input element and sending it keys
+describe('Testing if movieList works',()=>{
+    
+    test('displaying shrek',async()=>{
         await driver.findElement(By.xpath('//input')).sendKeys('Shrek\n')
         await driver.sleep(2000)
-        await driver.findElement(By.xpath('//input')).sendKeys('Shrek 2\n')
-        await driver.sleep(2000)
-        await driver.findElement(By.xpath('(//main/ul/li/span)[2]')).click()
-        // await driver.sleep(2000)
-
-        const isDisplayed = await driver.findElement(By.xpath('//main/aside')).isDisplayed()
-        expect(isDisplayed).toBeTruthy()
-        await driver.sleep(2000)
-
-
-        await driver.findElement(By.xpath('(//main/ul/li/button)[2]')).click()
-        await driver.sleep(2000)
-
+        const movie = await driver.findElement(By.xpath('//li'))
+        const displayed = movie.isDisplayed()
+        expect(displayed).toBeTruthy()
     })
+    
+    test('shrek 2 display',async()=>{
+        await driver.findElement(By.xpath('//input')).sendKeys('Shrek 2 \n')
+        await driver.sleep(2000)
+        const movie = await driver.findElement(By.xpath('//li'))
+        const displayed = movie.isDisplayed()
+        expect(displayed).toBeTruthy()
+    })
+
+    test('shrek 3 display' ,async()=>{
+        await driver.findElement(By.xpath('//input')).sendKeys('Shrek 3 \n')
+        await driver.sleep(2000)
+        const movie = await driver.findElement(By.xpath('//li'))
+        const displayed = movie.isDisplayed()
+        expect(displayed).toBeTruthy()
+    })
+
+    test('shrek 4 display',async()=>{
+        await driver.findElement(By.xpath('//input')).sendKeys('Shrek 4\n')
+        await driver.sleep(2000)
+        const movie = await driver.findElement(By.xpath('//li'))
+        const displayed = movie.isDisplayed()
+        expect(displayed).toBeTruthy()
+    })
+
+    test('cross off works',async()=>{
+        await driver.findElement(By.xpath('//li//span')).click()
+        await driver.sleep(2000)
+        let checked =  await driver.findElement(By.xpath('//li//span')).getAttribute('class')
+        expect(checked).toBe('checked')
+    })
+
+    test('uncross off works',async()=>{
+        await driver.findElement(By.xpath('//li//span')).click()
+        await driver.sleep(2000)
+        let checked =  await driver.findElement(By.xpath('//li//span')).getAttribute('class')
+        expect(checked).toBe('')
+    })
+
+    test('crossed off message works',async()=>{
+        await driver.findElement(By.xpath('//li//span')).click()
+        const displayed = await driver.findElement(By.xpath('//main/aside')).getText()
+        expect(displayed).toBe('Shrek watched!')
+    })
+
+    test('uncrossed off message works',async()=>{
+        await driver.findElement(By.xpath('//li//span')).click()
+        const displayed = await driver.findElement(By.xpath('//main/aside')).getText()
+        expect(displayed).toBe('Shrek added back!')
+    })
+
+    test('delete Shrek 2 works',async()=>{
+        await driver.findElement(By.xpath('(//main/ul/li/button)')).click()
+        await driver.sleep(2000)
+        const input = await driver.findElement(By.xpath('//input')).getText()
+        expect(input).toBe('')
+    })
+
+    
+
+
 })
-
-
-        //selectint the button and clicking it
-        // await driver.findElement(By.xpath('//button')).click()
-        
-
-        // const movie = await driver.findElement(By.xpath('//li'))
-
-        // const displayed = movie.isDisplayed()
-
-        // expect(displayed).toBeTruthy()
-        // await driver
